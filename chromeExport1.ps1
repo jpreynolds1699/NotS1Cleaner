26036ab2d720761b8d06e629807d6401bf0c4bb1
jpreynolds1699 <141958316+jpreynolds1699@users.noreply.github.com>
2025-04-24 12:38:00 -0400

    Update chromeExport1.ps1

--- chromeExport1.ps1
+++ chromeExport1.ps1
@@ -1,17 +1,17 @@
-$Path = "$Env:systemdrive\Users\swilson\AppData\Local\Google\Chrome\User Data\Default\History"
-if (-not (Test-Path -Path $Path)) {
-    Write-Verbose "[!] Could not find Chrome History for username: swilson"
-    }
-    $Regex = '(htt(p|s))://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
-    $Value = Get-Content -Path "$Env:systemdrive\Users\swilson\AppData\Local\Google\Chrome\User Data\Default\History"|Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
-    $Value | ForEach-Object {
-        $Key = $_
-        if ($Key -match $Search){
-            New-Object -TypeName PSObject -Property @{
-                User = $UserName
-                Browser = 'Chrome'
-                DataType = 'History'
-                Data = $_
-            }
+$Regex = '(htt(p|s))://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
+$Value = Get-Content -Path "$Env:systemdrive\Users\swilson\AppData\Local\Google\Chrome\User Data\Default\History"|Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
+
+$Results = @()
+$Value | ForEach-Object {
+    $Key = $_
+    if ($Key -match $Search){
+        $Results += New-Object -TypeName PSObject -Property @{
+            User = $UserName
+            Browser = 'Chrome'
+            DataType = 'History'
+            Data = $_
         }
     }
+}
+
+$Results | Export-Csv -Path "C:\ChromeHistoy.csv" -NoTypeInformation
